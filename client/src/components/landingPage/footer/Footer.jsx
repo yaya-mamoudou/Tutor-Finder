6e9e40fca98d5339caeab1fe375411a04af33799
yaya-mamoudou <yayamamoudou0@gmail.com>
2021-05-18 23:50:46 +0100

    Ended the landing Page

---
+++ client/src/components/landingPage/footer/Footer.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="d-flex" style={{ backgroundColor: "#2f2e41" }}>
+        <ul className="footerList list-style-none text-white">
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+        </ul>
+        <ul className="footerList list-style-none text-white">
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+        </ul>
+        <ul className="footerList list-style-none text-white">
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+        </ul>
+        <ul className="footerList list-style-none text-white">
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+        </ul>
+
+        <ul className="footerList list-style-none text-white">
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+        </ul>
+        <ul className="footerList list-style-none text-white">
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+        </ul>
+        <ul className="footerList list-style-none text-white">
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+          <li>Yaya</li>
+        </ul>
+      </div>
+      <div className="legend text-center">
+        <p>All Copyrights reserved @ Athena Global Education 2021</p>
+      </div>
+    </div>
+  );
+}
