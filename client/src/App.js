8063bbfc644dc20751ba43875e2d56c9a2ce0496
alicendeh <alicendeh16@gmail.com>
2021-05-19 01:29:56 +0100

    error1

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import './App.css';
-// import MainApp from "./components/mainApp/MainApp";
-import LandingPage from './components/landingPage/LandingPage';
-=======
-import "./App.css";
-import MainApp from "./components/mainApp/MainApp";
+import MainApp from './components/mainApp/MainApp';
 // import LandingPage from "./components/landingPage/LandingPage";
->>>>>>> 4a583c19ce7b33d9987eddf013243290c6d911bf
 function App() {
   return (
     <div className="App">
-      {/* <MainApp /> */}
-      <LandingPage />
+      <MainApp />
+      {/* <LandingPage /> */}
     </div>
   );
 }
