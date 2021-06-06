c8b8336b1b04c4011f723ebf2288f51b9fd4e236
yaya-mamoudou <yayamamoudou0@gmail.com>
2021-06-06 14:17:04 +0100

    icon tweek

--- client/src/components/mainApp/sideNav/navIcons/NavIcons.jsx
+++ client/src/components/mainApp/sideNav/navIcons/NavIcons.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export default function NavIcons({ id, icon, className, label, _setFocus }) {
   return (
     <div onClick={() => _setFocus(id)} key={id} className="d-flex">
       <div className={className}>
-        <i className={`icon ${icon}`}></i>
+        <i className={`icons ${icon}`}></i>
       </div>
       <div className="label">
         <span
-          className={className === 'iconBoxColor' ? 'labelColor' : 'text-white'}
+          className={className === "iconBoxColor" ? "labelColor" : "text-white"}
         >
           {label}
         </span>
