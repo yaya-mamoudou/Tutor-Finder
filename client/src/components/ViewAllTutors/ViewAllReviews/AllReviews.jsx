eafc8546ce0aaa0cc9a8ebf21a2e87a7ad7b1527
yaya-mamoudou <yayamamoudou0@gmail.com>
2021-06-11 17:09:37 +0100

    tutor profile done

--- client/src/components/ViewAllTutors/ViewAllReviews/AllReviews.jsx
+++ client/src/components/ViewAllTutors/ViewAllReviews/AllReviews.jsx
@@ -3,7 +3,6 @@ import ReviewContext from "../../../context/reviews/ReviewContext";
 import Review from "../reviews/Review";
 import { format } from "timeago.js";
 import DisplayRatings from "./DisplayRatings";
-
 import reviewerPic from "../../assets/img/1.jpg";
 
 function AllReviews(props) {
@@ -29,7 +28,7 @@ function AllReviews(props) {
     <div className="">
       {typeof tutData === "object" &&
         tutData.map((tutDset) => (
-          <div className=" mb-4 p-4 d-flex ">
+          <div className="p-2 d-flex ">
             <img
               src={reviewerPic}
               width="40"
@@ -38,15 +37,23 @@ function AllReviews(props) {
               alt=""
             />
             <div className="p-3 reviewTextBox w-100 ml-2">
-              <p className="font-weight-bold d-flex">
-                {tutDset.reviewers_id.username}
+              <p className=" d-flex">
+                <span className="font-weight-bold">
+                  {tutDset.reviewers_id.username}
+                </span>
 
                 <DisplayRatings rating={tutDset.rating} />
               </p>
-              <p>{tutDset.body}</p>
+              <p className="d-flex">
+                {tutDset.body}{" "}
+                <span
+                  style={{ fontSize: 9 }}
+                  className="text-secondary ml-auto"
+                >
+                  {format(tutDset.date)}
+                </span>
+              </p>
             </div>
-
-            <p className="text-light"> {format(tutDset.date)} </p>
           </div>
         ))}
     </div>
