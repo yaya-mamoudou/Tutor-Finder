961266c300ea9a0e1c9e9e79cb1a2931264f0f66
yaya-mamoudou <yayamamoudou0@gmail.com>
2021-06-12 12:58:32 +0100

    check this out

--- client/src/components/ViewAllTutors/TutorProfileHeader.jsx
+++ client/src/components/ViewAllTutors/TutorProfileHeader.jsx
@@ -1,24 +1,16 @@
-import React, { useEffect, useState, createRef } from "react";
+import React, { useEffect, useState } from "react";
 
 // console.log(Object.is(activePath, "/tut/profile"));
-export default function TutorProfileHeader({ status, setref }) {
+export default function TutorProfileHeader({ status }) {
   const [statusUser, setstatusUser] = useState("");
-  const headerRef = createRef();
 
   useEffect(async () => {
     let activePath = await window.location.pathname;
     await setstatusUser(activePath);
   }, []);
 
-  // useEffect(async () => {
-  //   if ((await headerRef.current) && headerRef.current.clientHeight) {
-  //     setref(headerRef.current.clientHeight);
-  //   } else {
-  //   }
-  // }, [headerRef]);
-
   return statusUser === "/tut/profile" ? (
-    <div ref={headerRef} className="d-flex mb-4">
+    <div className="d-flex mb-4">
       <p className="h3">Profile</p>
       <div className="ml-auto">
         <i class="far fa-comment-dots fa-2x"></i>
@@ -26,7 +18,7 @@ export default function TutorProfileHeader({ status, setref }) {
     </div>
   ) : (
     statusUser === "/profile" && (
-      <div ref={headerRef} className="d-flex mb-4">
+      <div className="d-flex mb-4">
         <p className="h3">Profile</p>
         <div className="ml-auto">
           <i class="far fa-edit fa-2x"></i>
