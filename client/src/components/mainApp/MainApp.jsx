443ac25f98a05e0002b8e06ecadfc1bc4d17fb88
Lontchi Frank <fodjof6@gmail.com>
2021-06-13 14:39:43 +0100

    .

--- client/src/components/mainApp/MainApp.jsx
+++ client/src/components/mainApp/MainApp.jsx
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./mainApp.css";
 import SideNav from "./sideNav/SideNav";
-import Chat from "./ChatPannel/Chat";
+// import Chat from "./ChatPannel/Chat";
 import Home from "../ViewAllTutors/ViewAllTutors";
 import Classroom from "./classroom/MainClassEntry";
 import Profile from "./Profile/Profile";
 import PrivateRoute from "../PrivateRouting/PrivateRoute";
 import TutorIndProfile from "../ViewAllTutors/TutorProfile";
 import ViewTutors from "./ViewTutors/ViewTutors";
-=======
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './mainApp.css';
-import SideNav from './sideNav/SideNav';
+
 import Chat from './chat/Chat';
-import Home from '../ViewAllTutors/ViewAllTutors';
-import Classroom from './classroom/MainClassEntry';
-import Profile from './Profile/Profile';
-import PrivateRoute from '../PrivateRouting/PrivateRoute';
-import TutorIndProfile from '../ViewAllTutors/TutorProfile';
->>>>>>> a89d7d8b84fe744cd021794bc7a5ccf51c8c38c2
 const Navbar = () => {
   return (
     <div className="main">
