6f4dbea7c73d3881ed31fd604ebe1c7e2ff8e80f
alicendeh <alicendeh16@gmail.com>
2021-06-14 00:38:59 +0100

    workked on the profile iimage

--- server/index.js
+++ server/index.js
@@ -2,26 +2,27 @@ const express = require('express');
 const athenaDB = require('./config/database');
 const multer = require('multer');
 const app = express();
+const path = require('path');
 
 athenaDB();
 
 const PORT = 5000;
 
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, 'hello.png');
+    cb(null, req.body.name);
   },
 });
 
 const upload = multer({ storage: storage });
-
-app.post('athena/file/upload', upload.single('file'), (req, res) => {
-  res.status(200).json('file uploaded succesfully');
+app.post('/athena/file/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
 });
 
 app.use('/athena/auth', require('./routes/auth'));
