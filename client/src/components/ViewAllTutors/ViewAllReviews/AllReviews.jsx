7eed545f6310dcd1ad7aa731873a7396b4fce717
alicendeh <alicendeh16@gmail.com>
2021-06-15 14:46:08 +0100

    new change

--- client/src/components/ViewAllTutors/ViewAllReviews/AllReviews.jsx
+++ client/src/components/ViewAllTutors/ViewAllReviews/AllReviews.jsx
@@ -9,12 +9,7 @@ const PF = 'http://localhost:5000/images/';
 
 function AllReviews(props) {
   const reviewContext = useContext(ReviewContext);
-<<<<<<< HEAD
-  const authContext = useContext(AuthContext);
 
-  const [pathname, setpathName] = useState();
-
-  let activePath;
   const {
     aTutsReview,
     viewATutR,
@@ -24,24 +19,10 @@ function AllReviews(props) {
   } = reviewContext;
   const [tutData, setTutData] = useState();
 
-  const [storeReview, setstoreReview] = useState();
-
-  useEffect(() => {
-    getMyReview();
-    setpathName(window.location.pathname);
-  }, [reviewContext]);
-  console.log(myReview);
-=======
-
-  const { aTutsReview, viewATutR, reviews, myReview, getMyReview } =
-    reviewContext;
-  const [tutData, setTutData] = useState();
-
   useEffect(() => {
     getMyReview();
   }, []);
 
->>>>>>> 7023285e429e57df7b260a8a44e8d3f85f38d909
   useEffect(async () => {
     try {
       let anID = await props.tut_id;
@@ -59,94 +40,16 @@ function AllReviews(props) {
     }
   }, [aTutsReview]);
 
-<<<<<<< HEAD
-  useEffect(async () => {
-    try {
-      await setstoreReview(myReview);
-    } catch (error) {}
-  }, [myReview]);
-
-  console.log(storeReview);
   return (
     <>
-      {pathname === '/profile' && typeof storeReview !== 'undefined'
-        ? storeReview.map((tutDset, index) => {
-            console.log(tutDset);
-            return (
-              <div className="p-2 d-flex ">
-                <img
-                  src={
-                    tutDset.reviewers_id.profilePic === ''
-                      ? 'http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico'
-                      : PF + tutDset.reviewers_id.profilePic
-                  }
-                  width="40"
-                  height="40"
-                  className="rounded-circle mt-1"
-                  alt=""
-                />
-                <div className="p-3 reviewTextBox w-100 ml-2">
-                  <p className=" d-flex">
-                    <DisplayRatings rating={tutDset.rating} />
-                  </p>
-                  <p className="d-flex">
-                    {tutDset.body}{' '}
-                    <span
-                      style={{ fontSize: 9 }}
-                      className="text-secondary ml-auto"
-                    >
-                      {format(tutDset.date)}
-                    </span>
-                  </p>
-                </div>
-              </div>
-            );
-          })
-        : pathname === '/tut/profile' &&
-          typeof tutData === 'object' &&
-          tutData.map((tutDset, index) => {
-            return (
-              <div className="p-2 d-flex ">
-                <img
-                  src={
-                    tutDset.reviewers_id.profilePic === ''
-                      ? 'http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico'
-                      : PF + tutDset.reviewers_id.profilePic
-                  }
-                  width="40"
-                  height="40"
-                  className="rounded-circle mt-1"
-                  alt=""
-                />
-                <div className="p-3 reviewTextBox w-100 ml-2">
-                  <p className=" d-flex">
-                    <span className="font-weight-bold">
-                      {tutDset.reviewers_id.username}
-                    </span>
-
-                    <DisplayRatings rating={tutDset.rating} />
-                  </p>
-                  <p className="d-flex">
-                    {tutDset.body}{' '}
-                    <span
-                      style={{ fontSize: 9 }}
-                      className="text-secondary ml-auto"
-                    >
-                      {format(tutDset.date)}
-                    </span>
-                  </p>
-                </div>
-=======
-  return (
-    <>
-      {typeof tutData === "object" &&
+      {typeof tutData === 'object' &&
         tutData.map((tutDset, index) => {
           return (
             <div className="reviewBox p-2 d-flex ">
               <img
                 src={
-                  tutDset.reviewers_id.profilePic === ""
-                    ? "http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico"
+                  tutDset.reviewers_id.profilePic === ''
+                    ? 'http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico'
                     : PF + tutDset.reviewers_id.profilePic
                 }
                 width="40"
@@ -163,7 +66,7 @@ function AllReviews(props) {
                   <DisplayRatings rating={tutDset.rating} />
                 </p>
                 <p className="d-flex">
-                  {tutDset.body}{" "}
+                  {tutDset.body}{' '}
                   <span
                     style={{ fontSize: 9 }}
                     className="text-secondary ml-auto"
@@ -171,7 +74,6 @@ function AllReviews(props) {
                     {format(tutDset.date)}
                   </span>
                 </p>
->>>>>>> 7023285e429e57df7b260a8a44e8d3f85f38d909
               </div>
             </div>
           );
