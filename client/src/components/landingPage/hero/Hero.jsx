8a68cb6842dd9517b02f2b8d8d182a181a9daf0c
yaya-mamoudou <yayamamoudou0@gmail.com>
2021-06-16 15:50:31 +0100

    ....

--- client/src/components/landingPage/hero/Hero.jsx
+++ client/src/components/landingPage/hero/Hero.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Nav from './nav/Nav';
-import TrustAndExperience from './TrustAndExperience';
-import Particles from 'react-particles-js';
+import React from "react";
+import { Link } from "react-router-dom";
+import Nav from "./nav/Nav";
+import TrustAndExperience from "./TrustAndExperience";
+import Particles from "react-particles-js";
 export default function Hero() {
   return (
     <div className="hero">
       <div className="heroDarkLayer d-flex container-fluid">
         <Nav />
         <div className="heroText text-center w-100 h-50  d-flex flex-column text-white justify-content-center">
-          <p className="font-weight-bold h5">Get in touch</p>
+          <p className="font-weight-bold h4">Get in touch</p>
           <TrustAndExperience />
 
-          <p className="mb-4 h6">
+          <p className="mb-4 h4">
             Tailored to your career goals and busy schedule
           </p>
           <Link to="/register">
-            <button className="btn EnrolButton ">Enrol now</button>
+            <button className="btn px-4 py-3 EnrolButton ">Enrol now</button>
           </Link>
         </div>
         <Particles
-          width={'100%'}
-          height={'100%'}
+          width={"100%"}
+          height={"100%"}
           params={{
             particles: {
               number: {
