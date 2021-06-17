0d8250763e8621ec096d0d111e6302d130a8c9dd
yaya-mamoudou <yayamamoudou0@gmail.com>
2021-06-17 15:38:13 +0100

    l

--- client/src/components/ViewAllTutors/FirstBox.jsx
+++ client/src/components/ViewAllTutors/FirstBox.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, useContext } from 'react';
-import ReviewContext from '../../context/reviews/ReviewContext';
+import React, { useEffect, useState, useContext } from "react";
+import ReviewContext from "../../context/reviews/ReviewContext";
 
-const PF = 'http://localhost:5000/images/';
+const PF = "http://localhost:5000/images/";
 
 export default function FirstBox({
   username,
@@ -11,17 +11,12 @@ export default function FirstBox({
   profilePic,
   status,
 }) {
-  let pp = localStorage.getItem('profilePic');
+  let pp = localStorage.getItem("profilePic");
 
   const reviewContext = useContext(ReviewContext);
 
-  const {
-    aTutsReview,
-    viewATutR,
-    reviews,
-    myReview,
-    getMyReview,
-  } = reviewContext;
+  const { aTutsReview, viewATutR, reviews, myReview, getMyReview } =
+    reviewContext;
   const [tutData, setTutData] = useState();
 
   const [pathname, setpathName] = useState();
@@ -33,49 +28,49 @@ export default function FirstBox({
 
   return (
     <div
-      className={`bg-white rounded ${status === 'learner' ? 'p-5' : 'p-4'} `}
-      style={{ height: '65%' }}
+      className={`bg-white rounded ${status === "learner" ? "p-5" : "p-4"} `}
+      style={{ height: "65%" }}
     >
       <div
         className={`${
-          status === 'tutor'
-            ? 'd-flex'
-            : 'd-flex flex-column align-items-center'
+          status === "tutor"
+            ? "d-flex"
+            : "d-flex flex-column align-items-center"
         }`}
       >
         <div
           style={{
-            width: status === 'learner' ? 80 : 55,
-            height: status === 'learner' ? 80 : 55,
+            width: status === "learner" ? 80 : 55,
+            height: status === "learner" ? 80 : 55,
           }}
           className={`bg-secondary d-flex ${
-            status === 'learner' && 'mb-4'
+            status === "learner" && "mb-4"
           } rounded-circle justify-content-center align-items-center`}
         >
-          {pathname === '/profile' ? (
+          {pathname === "/profile" ? (
             <img
               src={
-                profilePic === ''
-                  ? 'http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico'
+                profilePic === ""
+                  ? "http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico"
                   : PF + profilePic
               }
-              width={status === 'learner' ? '75' : '50'}
-              height={status === 'learner' ? '75' : '50'}
+              width={status === "learner" ? "75" : "50"}
+              height={status === "learner" ? "75" : "50"}
               className={`rounded-circle`}
               alt=""
             />
           ) : (
-            pathname === '/tut/profile' && (
+            pathname === "/tut/profile" && (
               <img
                 src={
-                  profilePic === ''
-                    ? 'http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico'
-                    : pp === ''
-                    ? 'http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico'
+                  profilePic === ""
+                    ? "http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico"
+                    : pp === ""
+                    ? "http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico"
                     : PF + pp
                 }
-                width={status === 'learner' ? '75' : '50'}
-                height={status === 'learner' ? '75' : '50'}
+                width={status === "learner" ? "75" : "50"}
+                height={status === "learner" ? "75" : "50"}
                 className={`rounded-circle`}
                 alt=""
               />
@@ -83,19 +78,20 @@ export default function FirstBox({
           )}
         </div>
         <div className="ml-3">
-          <p className={`${status === 'learner' && 'font-weight-bold fs-3'}`}>
+          <p className={`${status === "learner" && "font-weight-bold fs-3"}`}>
             {username}
           </p>
           <p className="text-danger">{speciality}</p>
         </div>
       </div>
-      <div style={{ overflowY: 'auto', maxHeight: '70%' }}>
+      <div style={{ overflowY: "auto", maxHeight: "70%" }}>
         <p className="text-secondary mt-4">Location</p>
         <p className="text-danger">{location}</p>
+
         <div className="mt-3">
           <p className="text-secondary">Ratings</p>
           <p className="text-danger">
-            4.5{' '}
+            4.5{" "}
             <span className="ml-2 text-danger">
               <i class="fas fa-star "></i>
               <i class="fas fa-star"></i>
@@ -105,6 +101,7 @@ export default function FirstBox({
             </span>
           </p>
         </div>
+
         <p className="text-secondary">About</p>
 
         <p className="p-2 pr-4" style={{ wordSpacing: 1.5, lineHeight: 1.6 }}>
