6d041bc9a9aaf3c8e00700188c0783333a5ac3db
alicendeh <alicendeh16@gmail.com>
2021-06-17 06:34:54 +0100

    working on classroom chat

--- client/src/components/mainApp/classroom/ClassChat.jsx
+++ client/src/components/mainApp/classroom/ClassChat.jsx
@@ -2,8 +2,11 @@ import React, { useContext, useState, useEffect } from 'react';
 import { FiMoreVertical } from 'react-icons/fi';
 import './classroom.css';
 import AuthContext from '../../../context/auth/AuthContext';
+import { format } from 'timeago.js';
 
 function ClassChat() {
+  const PF = 'http://localhost:5000/images/';
+
   const authContext = useContext(AuthContext);
 
   const {
@@ -11,6 +14,7 @@ function ClassChat() {
     createAClassMessage,
     getClassMsg,
     getClassMessages,
+    user,
   } = authContext;
 
   useEffect(async () => {
@@ -20,7 +24,7 @@ function ClassChat() {
     } catch (error) {
       console.log(error);
     }
-  }, [localStorage.getItem('CLASSID')]);
+  }, [localStorage.getItem('CLASSID'), classMessaging]);
 
   const [storeChat, setStoreChat] = useState();
 
@@ -31,8 +35,40 @@ function ClassChat() {
     <div>
       {typeof storeChat === 'object' &&
         storeChat.map((chatData) => (
-          <div>
-            <h2> {chatData.text} </h2>
+          <div className=" p-3  m-3">
+            <div
+              style={{
+                backgroundColor:
+                  chatData.sender[0]._id === user && user._id ? 'teal' : 'pink',
+                alignItems:
+                  chatData.sender[0]._id === user && user._id
+                    ? 'flex-end'
+                    : null,
+              }}
+            >
+              <div>
+                <div className="d-flex">
+                  <img
+                    src={
+                      PF + chatData.sender[0].profilePic === ''
+                        ? 'http://www.iconarchive.com/download/i102645/graphicloads/flat-finance/person.ico'
+                        : PF + chatData.sender[0].profilePic
+                    }
+                    alt=""
+                    height="50px"
+                    width="50px"
+                  />
+                  <h2 className="text-white">
+                    {' '}
+                    {chatData.sender[0].username}{' '}
+                  </h2>
+                </div>
+                <h2> {chatData.text} </h2>
+                <span className="text-secondary">
+                  {format(chatData.createdAt)}{' '}
+                </span>
+              </div>
+            </div>
           </div>
         ))}
     </div>
